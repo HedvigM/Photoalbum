fa23fc12388ff65a8f5d2e69fda683784db5cbc6
Hedvig Mejstedt <hedvig@mejstedt.se>
2024-02-11 21:02:51 +0100

    detailed image page

--- photoalbum/src/app/components/Images.tsx
+++ photoalbum/src/app/components/Images.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getPosts } from "../../../sanity/lib/queries";
 
 export async function ImagesContainer() {
@@ -9,6 +10,7 @@ export async function ImagesContainer() {
     image: string;
     alt: string;
     slug: string;
+    _id: string;
   };
 
   return (
@@ -16,22 +18,26 @@ export async function ImagesContainer() {
       style={{
         display: "grid",
         gridTemplateColumns: "repeat(3, 1fr)",
-        gap: "5px",
+        gap: "3px",
         overflow: "hidden",
       }}
     >
       {posts.map((post: Post) => (
-        <img
-          alt={post.alt}
-          src={post.image}
-          style={{
-            height: "100%",
-            width: "100%",
-            objectFit: "cover",
-            border: "1px solid black",
-            aspectRatio: "1/1",
-          }}
-        />
+        <>
+          <Link href={`/${post._id}`}>
+            <img
+              alt={post.alt}
+              src={post.image}
+              style={{
+                height: "100%",
+                width: "100%",
+                objectFit: "cover",
+                aspectRatio: "1/1",
+                borderRadius: "5px",
+              }}
+            />
+          </Link>
+        </>
       ))}
     </div>
   );
